af5f00d87951136ec965355d6fc672ec904e1d90
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-16 14:01:56 +0530

    Added login_request method in views

--- loginForm/login/views.py
+++ loginForm/login/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render
+
+from login.login_form import LoginForm
+
+
+def login_request(request):
+    form = LoginForm()
+    context = dict()
+    context['form'] = form
+    return render(request, 'login.html', context)
