0e7072a32cd09fc01e5e09aa9a051d3e3a755697
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-16 14:25:17 +0530

    Added login_validate method in views

--- loginForm/login/views.py
+++ loginForm/login/views.py
@@ -9,3 +9,11 @@ def login_request(request):
     context = dict()
     context['form'] = form
     return render(request, 'login.html', context)
+
+
+def login_validate(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['username'])
+            return render(request, 'user.html', {'username': form.cleaned_data['username']})
