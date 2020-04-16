3f1ba27fa23add325e82d93a7beaceb8c565b268
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-16 15:35:58 +0530

    Added validation method in form.

--- loginForm/login/login_form.py
+++ loginForm/login/login_form.py
@@ -1,6 +1,16 @@
 from django import forms
 
+from .models import UserDB
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_username(self):
+        name = self.cleaned_data['username']
+        user = UserDB.objects.filter(username=name)
+
+        if not user:
+            raise forms.ValidationError('User not registered')
+        return user
