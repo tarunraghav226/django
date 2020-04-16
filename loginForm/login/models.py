5c8b8bd3b5d1bbc8b61cfe7fc7a4edb6b8eae8ed
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-16 14:31:22 +0530

    Added user database.

--- loginForm/login/models.py
+++ loginForm/login/models.py
@@ -1,1 +1,10 @@
 # Create your models here.
+from django.db import models
+
+
+class UserDB(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
