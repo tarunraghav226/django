e0d25fcd27e388e3c086fdd99f7ebe67550b6fca
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-16 17:55:16 +0530

    Completed definition of login_form.

--- loginForm/login/login_form.py
+++ loginForm/login/login_form.py
@@ -7,13 +7,15 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput())
 
-    def clean_username(self):
-        name = self.cleaned_data['username']
-        user = UserDB.objects.filter(username=name)
+    def clean(self):
+        super(LoginForm, self).clean()
 
-        if not user:
+        dbuser = UserDB.objects.filter(username=self.cleaned_data.get('username'),
+                                       password=self.cleaned_data.get('password'))
+
+        if not dbuser:
             raise forms.ValidationError('User not registered')
-        return user
+        return self.cleaned_data
 
 
 class RegisterUser(forms.Form):
