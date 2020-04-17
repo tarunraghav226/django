3adcda6a85d0d1f12ce11723a7f1203cc3449fd4
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-17 15:29:26 +0530

    Updated form validation.

--- loginForm/login/forms.py
+++ loginForm/login/forms.py
@@ -2,22 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-from .models import UserDB
-
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput())
-
-    def clean(self):
-        super(LoginForm, self).clean()
-
-        dbuser = UserDB.objects.filter(username=self.cleaned_data.get('username'),
-                                       password=self.cleaned_data.get('password'))
-
-        if not dbuser:
-            raise forms.ValidationError('User not registered')
-        return self.cleaned_data
 
 
 class RegisterUser(forms.Form):
