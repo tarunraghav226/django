5b88b5429f157a259b48fc7bb029c0de3f9fcf2f
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-18 15:11:52 +0530

    Added new model

--- loginForm/login/models.py
+++ loginForm/login/models.py
@@ -1,10 +1,20 @@
 # Create your models here.
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class UserDB(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+
+class UserImage(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    dp = models.ImageField(upload_to='dp', null=True)
+    contest_photo = models.ImageField(upload_to='contest_photo', null=True)
 
     def __str__(self):
         return self.username
