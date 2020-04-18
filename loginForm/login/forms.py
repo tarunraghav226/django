15e7af7bf191aaa45bb04a1761c1c78e36879e05
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-18 15:13:05 +0530

    Added new image form.

--- loginForm/login/forms.py
+++ loginForm/login/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+from login.models import UserImage
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
@@ -38,9 +40,28 @@ class RegisterUser(forms.Form):
         return password2
 
     def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password1']
-        )
+
+        user = User()
+        user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
+        user.password = self.cleaned_data['password1']
+
+        if commit:
+            user.save()
         return user
+
+
+class UserImageForm(forms.ModelForm):
+    class Meta:
+        model = UserImage
+        fields = ('dp', 'contest_photo')
+
+    def save(self, username, commit=True):
+        image_user = UserImage()
+        image_user.username = username
+        image_user.dp = self.cleaned_data['dp']
+        image_user.contest_photo = self.cleaned_data['contest_photo']
+
+        if commit:
+            image_user.save()
+        return image_user
