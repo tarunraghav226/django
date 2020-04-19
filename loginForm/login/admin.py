cde6842b156af7a7af052a5fcf9c57742cde3e7f
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-19 16:35:09 +0530

    Registered LikedPhotos database.

--- loginForm/login/admin.py
+++ loginForm/login/admin.py
@@ -1,8 +1,10 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import UserDB, UserImage
+from .models import UserDB, UserImage, LikedPhotos
 
 admin.site.register(UserDB)
 
 admin.site.register(UserImage)
+
+admin.site.register(LikedPhotos)
